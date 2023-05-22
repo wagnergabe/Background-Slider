a31e83a71b438f9664a90a9b18edc4f2a0d2e1c2
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-05-21 21:03:52 -0500

    script somewhat working, need to fix repeat

---
+++ script.js
@@ -0,0 +1,31 @@
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+let activeSlide = 0;
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+
+    if(activeSlide > slides.length -1) {
+        activeSlide = 0
+    }
+
+    SetBgToBody();
+    setActiveSlide();
+})
+
+SetBgToBody();
+
+function SetBgToBody() {
+    body.style.background=slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide() {
+    slides.forEach(slide => {
+        slide.classList.remove('active');
+
+        slides[activeSlide].classList.add('active')
+    })
+}
